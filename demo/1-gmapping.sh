23e65879469159619ffaf5ae5e854e3b52e9fbab
forever-Victoria <3221313348@qq.com>
2025-12-03 09:58:49 +0800

    Add script for gmapping with terminal tabs

---
+++ demo/1-gmapping.sh
@@ -0,0 +1,6 @@
+###gmapping with abot###
+gnome-terminal --window -e 'bash -c "roscore; exec bash"' \
+--tab -e 'bash -c "sleep 3; source ~/454510_ws/devel/setup.bash; roslaunch abot_bringup robot_with_imu.launch; exec bash"' \
+--tab -e 'bash -c "sleep 4; source ~/454510_ws/devel/setup.bash; roslaunch robot_slam gmapping.launch; exec bash"' \
+--tab -e 'bash -c "sleep 4; source ~/454510_ws/devel/setup.bash; roslaunch robot_slam view_mapping.launch; exec bash"' \
+--tab -e 'bash -c "sleep 4; rosrun teleop_twist_keyboard teleop_twist_keyboard.py; exec bash"' \ 
